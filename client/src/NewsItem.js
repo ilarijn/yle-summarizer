4a5031f6a1ffeb0c1c67eec62a1838e4036c9863
Ilari Junnonen <ilari.junnonen@iki.fi>
2021-06-07 22:40:10 +0300

    Add caret icons

--- client/src/NewsItem.js
+++ client/src/NewsItem.js
@@ -11,7 +11,7 @@ import { useState } from "react"
 const NewsItem = ({ news }) => {
   const [summary, setSummary] = useState(null)
   const [showSummary, setShowSummary] = useState(true)
-  const [fullText, setFullText] = useState(news.content)
+  const [expanded, setExpanded] = useState(false)
 
   const summarize = async () => {
     const textContent = parseHtml(news.content)
@@ -28,11 +28,39 @@ const NewsItem = ({ news }) => {
           as={Button}
           variant="text"
           eventKey="0"
-          onClick={async () => summarize()}
+          onClick={async () => {
+            summarize()
+            setExpanded(!expanded)
+          }}
         >
           <Row>
             <Col xs={12} className="text-left">
-              <Card.Title className="news-title">{news.title}</Card.Title>
+              <Card.Title className="news-title">
+                {expanded ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-caret-up-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-caret-down-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                  </svg>
+                )}{" "}
+                {news.title}
+              </Card.Title>
               <Card.Subtitle className="mb-2 text-muted news-title">
                 {news.pubDate} <br />
               </Card.Subtitle>
@@ -60,7 +88,7 @@ const NewsItem = ({ news }) => {
                         return (
                           <div
                             className="full-text"
-                            dangerouslySetInnerHTML={{ __html: fullText }}
+                            dangerouslySetInnerHTML={{ __html: news.content }}
                           />
                         )
                     else if (showSummary) return <Spinner animation="border" />
@@ -68,7 +96,7 @@ const NewsItem = ({ news }) => {
                       return (
                         <div
                           className="full-text"
-                          dangerouslySetInnerHTML={{ __html: fullText }}
+                          dangerouslySetInnerHTML={{ __html: news.content }}
                         />
                       )
                   })()}
